3343aa034da8f834e7994b4e4994926e79d2b23c
subodh2711 <55016226+subodh2711@users.noreply.github.com>
2025-05-19 14:53:07 +0530

    Update cc_functions.js

--- blocks/form/cc_functions.js
+++ blocks/form/cc_functions.js
@@ -1,8 +1,8 @@
 import { getFullName1, getFullName3 } from './cc/cc1/cc2/functions';
 
 /**
- * Get Full Name 2
- * @name getFullName2 Concats first name and last name 2
+ * Get Full Name 5
+ * @name getFullName5 Concats first name and last name 5
  * @param {string} firstname in Stringformat
  * @param {string} lastname in Stringformat
  * @return {string}
